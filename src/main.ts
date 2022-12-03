6004e8da6edcaaf440b6c589a8af6fca192f5a02
Jackson Ramalho <jackson.pires.rm@gmail.com>
2022-12-03 18:53:54 -0300

    fix: deploy

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 import { initBot } from './bot';
 //import { googleCredentials } from './env';
 //import * as fs from "fs";
@@ -15,13 +15,13 @@ import { initBot } from './bot';
 // });
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // app.enableCors();
-  // const globalPrefix = 'api';
-  // app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
   await initBot();
-  // const port = process.env.PORT || 3333;
-  // await app.listen(port, '0.0.0.0');
+  const port = process.env.PORT || 3333;
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
